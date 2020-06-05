0b66b7f829778ca9885fe249f9a462310848bf38
Ridwan Onikoyi <onikoyiridwan@gmail.com>
2020-06-05 15:18:09 +0100

    Update HNG-03437-sweetheart.php

--- scripts/HNG-03437-sweetheart.php
+++ scripts/HNG-03437-sweetheart.php
@@ -1,12 +1,9 @@
 
-<?php
-            $fullname = "Amadikwa Joy";
+
+$fullname = "Amadikwa Joy";
             $id = "HNG-03437";
             $language = "PHP";
             $email = "amadikwajoyn@gmail.com"
             
                 
             echo("Hello World, this is ".$fullname." with HNGi7 ID ".$id." using ".$language." for stage 2 task ".$email);
-           ?>
-           
-
